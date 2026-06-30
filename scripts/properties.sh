1cd1a4484ea84676013e015b9e7bb59b7e384608
omar-haidar <rovox2000@gmail.com>
2026-06-30 03:24:57 -0500

    Update package name to dev.omar.xterminal

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -371,7 +371,7 @@ fi
 #
 # Default value: `Termux`
 ##
-TERMUX__NAME="Termux"
+TERMUX__NAME="XTerminal"
 
 ##
 # The lower case value for `TERMUX__NAME`.
@@ -464,7 +464,7 @@ TERMUX__REPOS_HOST_ORG_URL="https://github.com/$TERMUX__REPOS_HOST_ORG_NAME"
 #
 # Default value: `com.termux`
 ##
-TERMUX_APP__PACKAGE_NAME="com.termux"
+TERMUX_APP__PACKAGE_NAME="dev.omar.xterminal"
 TERMUX_APP_PACKAGE="$TERMUX_APP__PACKAGE_NAME" # Deprecated alternative variable for `TERMUX_APP__PACKAGE_NAME`
 
 __termux_build_props__add_variables_validator_actions "TERMUX_APP__PACKAGE_NAME" "app_package_name"
